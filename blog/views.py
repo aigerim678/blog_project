bfb48247bd6a72f36b811f0e3f541ecaebfa00bc
Aigerim Sabyrkhan <aigerimsabyrkhan393@gmail.com>
2024-09-28 21:17:21 +0300

    Card Component

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+
 from django.views.generic import ListView
 from .models import Post
 # Create your views here.
@@ -6,5 +6,6 @@ class HomeView(ListView):
     model = Post
 
     template_name = 'blog/index.html'
+    context_object_name = "posts"
 
     
